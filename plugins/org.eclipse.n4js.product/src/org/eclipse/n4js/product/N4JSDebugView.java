b910de92bcb252e64ab0101677bd296952d5fcdc
Emira Zorgati <emira.zorgati97@gmx.de>
2019-12-09 12:42:20 +0100

    Added debug and variable view
    Signed-off-by: Emira Zorgati <emira.zorgati97@gmx.de>

---
+++ plugins/org.eclipse.n4js.product/src/org/eclipse/n4js/product/N4JSDebugView.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2019 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.product;
+
+import org.eclipse.debug.ui.AbstractDebugView;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ *
+ */
+public class N4JSDebugView extends AbstractDebugView {
+
+	@Override
+	protected Viewer createViewer(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void createActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getHelpContextId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void fillContextMenu(IMenuManager menu) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void configureToolBar(IToolBarManager tbm) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ plugins/org.eclipse.n4js.product/src/org/eclipse/n4js/product/N4JSVariableView.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2019 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.product;
+
+import org.eclipse.debug.internal.ui.views.variables.VariablesView;
+
+/**
+ *
+ */
+public class N4JSVariableView extends VariablesView {
+
+}
